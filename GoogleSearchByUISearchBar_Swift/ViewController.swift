db78df537c4f0512441b3e464a221ded74b9debd
岡　大輔（Daisuke Oka) <daisuke_oka@nanosoftware.biz>
2015-03-01 08:54:38 +0900

    途中

--- GoogleSearchByUISearchBar_Swift/ViewController.swift
+++ GoogleSearchByUISearchBar_Swift/ViewController.swift
@@ -8,16 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UISearchBarDelegate,UIPickerViewDelegate,UIPickerViewDataSource{
 
+    @IBOutlet weak var mySearchBar: UISearchBar!
+
+    @IBOutlet weak var myPickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.myPickerView.delegate = self
+        self.mySearchBar.delegate = self
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        if searchBar.text == "" {
+            return
+        }
+        
+        var site:CountryOfGoogleSite
+        
+        if self.myPickerView.selectedRowInComponent(0) == 0 {
+            site = .JapaneseGoogle
+            
+        }else if self.myPickerView.selectedRowInComponent(0) == 1{
+            site = .AmericalGoogle
+        }else{
+            site = .JapaneseGoogle
+        }
+        
+        var ViewController:GoogleSearchViewColtroller = GoogleSearchViewColtroller()
+        ViewController.googlesite = site
+        ViewController.SearchString = searchBar.text
+        ViewController.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        self.presentViewController(ViewController, animated: false, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        if(row == 0){
+            return "日本のGoogle"
+        }else if(row == 1){
+            return "American Google"
+        }
+        
+        return ""
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 2
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
     }
 
 
