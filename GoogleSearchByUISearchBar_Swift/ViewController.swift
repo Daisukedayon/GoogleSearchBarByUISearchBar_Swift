eeaecb956d9dda6ea184fe3fad5e0e14b021ef1c
岡　大輔（Daisuke Oka) <daisuke_oka@nanosoftware.biz>
2015-03-01 06:45:17 +0900

    Initial Commit

---
+++ GoogleSearchByUISearchBar_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleSearchByUISearchBar_Swift
+//
+//  Created by 岡大輔 on 2015/03/01.
+//  Copyright (c) 2015年 岡大輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
